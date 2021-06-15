53a41670b4b3ea252a6d95f8105cfbc75542a0f8
shabobble <pksullivan@yahoo.com>
2021-06-15 12:33:11 -0400

    changed port on server

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
